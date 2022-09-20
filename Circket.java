ed2f538f1cb0c7a10302d5fb85ceff7bd70693d6
karthiknns <113502169+karthiknns@users.noreply.github.com>
2022-09-20 18:02:04 +0530

    Add files via upload

---
+++ Circket.java
@@ -0,0 +1,119 @@
+class Circket{
+	
+	static int teamPlayersPerMatch;
+	static int exraPlayers;
+	static String mainBoard;
+	static String format;
+	static String[] teams;
+	static String[] indianTeam;
+	static String[] australiaTeam;
+	static String[] englandTeam;
+	static String[] southAfricaTeam;
+	static String[] netherLandTeam;
+	static String[] namibiaTeam;
+	static String[] bangladeshTeam;
+	static String[] westindiesTeam;
+	static String[] afganistanTeam;
+	static String[] pakistanTeam;
+	static String[] zimbambeTeam;
+	static String[] srilankaTeam;
+	static String[] uaeTeam;
+	static String[] newzealandteam;
+	
+	static void circketTeam()
+	{
+		System.out.println(teamPlayersPerMatch);
+		System.out.println(exraPlayers);
+		System.out.println(mainBoard);
+		System.out.println(format);
+		for(int team1=0;team1<teams.length;team1++)
+		{
+			String ref1=teams[team1];
+			System.out.println("...."+ref1);
+		}
+		System.out.println("india------------------");
+		for(int team2=0;team2<indianTeam.length;team2++)
+		{
+			String ref2=indianTeam[team2];
+			System.out.println("...."+ref2);
+		}
+		System.out.println("australia---------------");
+		for(int team3=0;team3<australiaTeam.length;team3++)
+		{
+			String ref3=australiaTeam[team3];
+			System.out.println("....,"+ref3);
+		}
+		System.out.println("england--------------");
+		for(int team4=0;team4<englandTeam.length;team4++)
+		{
+			String ref4=englandTeam[team4];
+			System.out.println("....,"+ref4);
+		}
+		System.out.println("southAfrica---------------");
+		for(int team5=0;team5<southAfricaTeam.length;team5++)
+		{
+			String ref5=southAfricaTeam[team5];
+			System.out.println("....,"+ref5);
+		}
+		System.out.println("netherLand---------------------");
+		for(int team6=0;team6<netherLandTeam.length;team6++)
+		{
+			String ref6=netherLandTeam[team6];
+			System.out.println("....,"+ref6);
+		}
+		System.out.println("namibia---------------------");
+		for(int team7=0;team7<namibiaTeam.length;team7++)
+		{
+			String ref7=namibiaTeam[team7];
+			System.out.println("....,"+ref7);
+		}
+		System.out.println("bangladesh---------------------");
+		for(int team8=0;team8<bangladeshTeam.length;team8++)
+		{
+			String ref8=bangladeshTeam[team8];
+			System.out.println("....,"+ref8);
+		}
+		System.out.println("westindies---------------------");
+		for(int team9=0;team9<westindiesTeam.length;team9++)
+		{
+			String ref9=westindiesTeam[team9];
+			System.out.println("....,"+ref9);
+		}
+		System.out.println("afganistan---------------------");
+		for(int team10=0;team10<afganistanTeam.length;team10++)
+		{
+			String ref10=afganistanTeam[team10];
+			System.out.println("....,"+ref10);
+		}
+		System.out.println("pakistan---------------------");
+		for(int team11=0;team11<pakistanTeam.length;team11++)
+		{
+			String ref11=pakistanTeam[team11];
+			System.out.println("....,"+ref11);
+		}
+		System.out.println("zimbambe---------------------");
+		for(int team12=0;team12<zimbambeTeam.length;team12++)
+		{
+			String ref12=zimbambeTeam[team12];
+			System.out.println("....,"+ref12);
+		}
+		System.out.println("srilanka---------------------");
+		for(int team13=0;team13<srilankaTeam.length;team13++)
+		{
+			String ref13=srilankaTeam[team13];
+			System.out.println("....,"+ref13);
+		}
+		System.out.println("uae---------------------");
+		for(int team14=0;team14<uaeTeam.length;team14++)
+		{
+			String ref14=uaeTeam[team14];
+			System.out.println("....,"+ref14);
+		}
+		System.out.println("newzealand---------------------");
+		for(int team15=0;team15<newzealandteam.length;team15++)
+		{
+			String ref15=newzealandteam[team15];
+			System.out.println("....,"+ref15);
+		}
+	}
+	}
---
+++ CircketRunner.java
@@ -0,0 +1,44 @@
+class CircketRunner{
+	public static void main(String[] args)
+	{
+		Circket.teamPlayersPerMatch=11;
+		Circket.exraPlayers=4;
+		Circket.mainBoard="ICC";
+		Circket.format="T20";
+		String[] t={"india","australia","england","southAfrica","netherLand","namibia","bangladesh","westindies","afganistan","pakistan","zimbambe","srilanka","uae","newzealand"};
+		Circket.teams=t;
+		String[] t2={"rohith","rahul","kohli","s.yadav","d.hooda","m,shami","karthik","bumrah","h,patel","b.kumar","a.singh"};
+		Circket.indianTeam=t2;
+		String[] t3={"finch","david","marsh","smith","wade","hazlewood","maxwell","starc","warner","cummins","stonis"};
+		Circket.australiaTeam=t3;
+		String[] t4={"buttler","ali","bairstow","curran","jordon","livingstone","malan","rashid","salt","stokes","topley"};
+		Circket.englandTeam=t4;
+		String[] t5={"bavuma","de kock","klaasen","hendricks","maharaj","markram","miller","ngdi","nortje","rabada","shamsi"};
+		Circket.southAfricaTeam=t5;
+		String[] t6={"edwards","van beek","van der gugten","singh","cooper","klaassen","van der merwe","m.o'dowd","ahamd","glower","pringle"};
+		Circket.netherLandTeam=t6;
+		String[] t7={"erasmus","wiese","van lingen","smit","shikongo","la cock","green","scholtz","baard","frylinck","louwrens","ya france"};
+		Circket.namibiaTeam=t7;
+		String[] t8={"s.al hasan","shanto","rahman","miraz","hossain","ahmed","daa","rahman","mahmud","ahmed","islam","hossain"};
+		Circket.bangladeshTeam=t8;
+		String[] t9={"pooran","powell","cariah","charles","cottrell","hetmyer","holder","hosein","joseph","lewis"};
+		Circket.westindiesTeam=t9;
+        String[] t10={"nabi","malik","haq","zadran","farooqi","ahmad","gurbaz","zazai","r.khan","shah","rasooli"};
+		Circket.afganistanTeam=t10;
+		String[] t11={"b.azam","ahmed","wasim","s.khan","k.shah","n.shah","a.ali","h.ali","m.nawaz","m.rizwan","s.dhani"};
+		Circket.pakistanTeam=t11;
+		String[] t12={"c.ervine","r.burl","chakabva","chatara","evans","jongew","madande","madhevere","r.ngarava","i.kaia","k.kasuza"};
+		Circket.zimbambeTeam=t12;
+		String[] t13={"d.shanaka","rajpaksa","karunaratne","ganathilaka","d.de silva","d.chameera","p.nissanka","k.mendis","vandersay","l.kumara"};
+		Circket.srilankaTeam=t13;
+		String[] t14={"rizwan","lakra","khan","aravind","farid","s.ali","k.daud","a.raza","shetty","nawaz","s.ahmed","k.meiyappan"};
+		Circket.uaeTeam=t14;
+		String[] t15={"k.williomson","santner","fin allen","t latham","j.neesham","D.conway","t.southee","t.boult","i.sodhi","l.ferguson"};
+		Circket.newzealandteam=t15;
+		
+		
+		
+		
+		Circket.circketTeam();
+	}
+}
