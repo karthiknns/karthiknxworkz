f94aeecdd80f2cd811ac71c27b34341774c22d58
karthiknns <113502169+karthiknns@users.noreply.github.com>
2022-09-22 17:59:27 +0530

    Add files via upload

---
+++ ChiefMinister,java.java
@@ -0,0 +1,39 @@
+class  ChiefMinister{
+	public static void main(String[] value)
+	{
+		String ref=value[0];
+		String ref1=value[1];
+		String ref2=value[2];
+		String ref3=value[3];
+		String ref4=value[4];
+		String ref5=value[5];
+		String ref6=value[6];
+		String ref7=value[7];
+		String ref8=value[8];
+		String ref9=value[9];
+		
+		System.out.println("total  ="+value.length);
+		
+		System.out.println("name ="+ref);
+		
+		System.out.println("age ="+ref1);
+		
+		System.out.println("education ="+ref2);
+		
+		System.out.println("state ="+ref3);
+		
+		System.out.println("party ="+ref4);
+		
+		System.out.println("majority to become cm ="+ref5);
+		
+		System.out.println("experence in party ="+ref6);
+		
+		System.out.println("home town ="+ref7);
+		
+		System.out.println("how may times he is cm ="+ref8);
+		
+		System.out.println("from which year he is cm ="+ref9);
+		
+		
+	}
+}
---
+++ SoftwareCompany.java
@@ -0,0 +1,15 @@
+class SoftwareCompany{
+
+public static void main(String[] value)
+{
+	for(int ref=0;ref<value.length;ref++)
+	{
+		String ref1=value[ref];
+		System.out.println("Software company Details"+ref1);
+	}
+	
+	System.out.println("total length  ----"+value.length);
+}
+
+
+}
