6d600f076d677f4c920c51ecf4fc961ac0b1c34d
karthiknns <113502169+karthiknns@users.noreply.github.com>
2022-09-22 18:29:27 +0530

    Add files via upload

---
+++ HeritageSitesNames.java
@@ -0,0 +1,33 @@
+class HeritageSitesNames{
+	
+	public static void main(String[] sites)
+	{
+		
+		String ref=sites[0];
+		String ref1=sites[1];
+		String ref2=sites[2];
+		String ref3=sites[3];
+		String ref4=sites[4];
+		String ref5=sites[5];
+		String ref6=sites[6];
+		String ref7=sites[7];
+		String ref8=sites[8];
+		String ref9=sites[9];
+		String ref10=sites[10];
+		System.out.println("heritage site names----"+ref);
+         		System.out.println("heritage site names----"+ref1);
+			System.out.println("heritage site names----"+ref2);
+			System.out.println("heritage site names----"+ref3);
+			System.out.println("heritage site names----"+ref4);
+			System.out.println("heritage site names----"+ref5);
+			System.out.println("heritage site names----"+ref6);
+			System.out.println("heritage site names----"+ref7);
+			System.out.println("heritage site names----"+ref8);
+			System.out.println("heritage site names----"+ref9);
+			System.out.println("heritage site names----"+ref10);
+		
+		
+		 System.out.println("total  -  -  -  -"+sites.length);
+		}
+	
+}
---
+++ RiverNames.java
@@ -0,0 +1,33 @@
+class RiverNames{
+	
+	public static void main(String[] names)
+	{
+		String ref=names[0];
+		String ref1=names[1];
+		String ref2=names[2];
+		String ref3=names[3];
+		String ref4=names[4];
+		String ref5=names[5];
+		String ref6=names[6];
+		String ref7=names[7];
+		String ref8=names[8];
+		String ref9=names[9];
+		
+		System.out.println("river names+=-----"+ref);
+		System.out.println("river names+=-----"+ref1);
+		System.out.println("river names+=-----"+ref2);
+		System.out.println("river names+=-----"+ref3);
+		System.out.println("river names+=-----"+ref4);
+		System.out.println("river names+=-----"+ref5);
+		System.out.println("river names+=-----"+ref6);
+		System.out.println("river names+=-----"+ref7);
+		System.out.println("river names+=-----"+ref8);
+		System.out.println("river names+=-----"+ref9);
+		
+		
+		
+		System.out.println("total river-----"+names.length);
+		
+		
+	}
+}
